64b9ac6fc9237c6e8a9c9a4410f8e27e767d0abe
Alejandro Pizarro Medina <a01566565@tec.mx>
2024-08-28 18:36:36 -0600

    Commited step5

--- routes/temperature.routes.js
+++ routes/temperature.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { check, param } = require('express-validator');
 const temperatureController = require("../controllers/temperature.controller");
 
-router.post("/v1/temperature/convert/:unitToConvert", [
+router.post("/v1/temperatures/convert/:unitToConvert", [
         check("value").notEmpty().withMessage("value is mandatory"),
         check("value").isNumeric().withMessage("values must be a number"),
         check("unit").notEmpty().withMessage("unit is mandatory"),
